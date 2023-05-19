8d02da3b9a557d342ac7982e31838315a923e48f
Yudota <yuri.fernandes.961@gmail.com>
2023-05-19 04:27:45 -0300

    feat(mobile): pagina inicial

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+  if (!hasLoadedFonts) return null
   return (
-    <View className="bg-gray-950 items-center justify-center flex-1">
-      <Text className="text-zinc-50 text-4xl font-bold">Hello World</Text>
-      <StatusBar style="light" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 px-8 py-10 bg-gray-900"
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
+      <StyledStripes className="left-2 absolute" />
+
+      <View className="items-center justify-center flex-1 gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="font-title text-gray-50 text-2xl leading-tight text-center">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="font-body leading- relaxed text-base text-center text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="px-5 py-2 bg-green-500 rounded-full"
+        >
+          <Text className="font-alt text-sm text-black uppercase">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="font-body text-sm leading-relaxed text-center text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   )
 }
